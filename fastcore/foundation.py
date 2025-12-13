6e263f2552ad1596e05a73d6387b2437e55faf30
Jeremy Howard <github@jhoward.fastmail.fm>
2025-12-13 14:21:43 +1000

    rename

--- fastcore/foundation.py
+++ fastcore/foundation.py
@@ -493,7 +493,7 @@ except ImportError: batched = _batched
 
 # %% ../nbs/02_foundation.ipynb
 @patch
-def batched(self:L, n):
+def batched__(self:L, n):
     "Same as `itertools.batched`"
     return self._new(batched(self, n))
 
